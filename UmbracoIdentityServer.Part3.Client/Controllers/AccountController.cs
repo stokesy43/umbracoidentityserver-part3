4860f76f7282eb7cb3d4453728be5f14fdff2fb6
Peter Stokes <stokesy43@gmail.com>
2018-04-09 16:27:32 +0100

    add project files

---
+++ UmbracoIdentityServer.Part3.Client/Controllers/AccountController.cs
@@ -0,0 +1,89 @@
+﻿using IdentityModel;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Core;
+using Umbraco.Web;
+using Umbraco.Web.Mvc;
+
+namespace UmbracoIdentityServer.Part3.Client.Controllers
+{
+    public class AccountController : SurfaceController
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult ExternalLogin(string provider, string returnUrl = null)
+        {
+            if (provider.IsNullOrWhiteSpace() && returnUrl.IsNullOrWhiteSpace())
+            {
+                return Redirect("/account");
+            }
+
+            if (returnUrl.IsNullOrWhiteSpace())
+            {
+                returnUrl = Request.RawUrl;
+            }
+
+            HttpContext.GetOwinContext().Authentication.Challenge(new AuthenticationProperties
+            {
+                RedirectUri = Url.SurfaceAction<AccountController>("ExternalLoginCallback", new { ReturnUrl = returnUrl })
+            }, provider);
+
+            return new HttpUnauthorizedResult();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<ActionResult> ExternalLoginCallback(string returnUrl)
+        {
+            // First get the authentication information from identity server
+            var loginInfo = await HttpContext.GetOwinContext().Authentication.GetExternalLoginInfoAsync();
+
+            if (loginInfo == null)
+            {
+                //go home, invalid callback
+                return RedirectToLocal(returnUrl);
+            }
+
+            // Get the relevant claims from the external identity
+            var email = loginInfo.ExternalIdentity.Claims.First(c => c.Type == JwtClaimTypes.Email).Value;
+            var name = loginInfo.ExternalIdentity.Claims.First(c => c.Type == JwtClaimTypes.Name).Value;
+            var role = loginInfo.ExternalIdentity.Claims.First(c => c.Type == JwtClaimTypes.Role).Value;
+            
+            // Sign out externally
+            HttpContext.GetOwinContext().Authentication.SignOut(DefaultAuthenticationTypes.ExternalCookie);
+
+            // Create a local claims identity copying the claims provided from the external provider
+            var claims = new List<Claim>
+            {
+                new Claim(JwtClaimTypes.Email, email),
+                new Claim(JwtClaimTypes.Name, name),
+                new Claim(JwtClaimTypes.Role, role),
+            };
+
+            var id = new ClaimsIdentity(DefaultAuthenticationTypes.ApplicationCookie, JwtClaimTypes.Email, JwtClaimTypes.Role);
+            id.AddClaims(claims);
+
+            // Sign in locally
+            HttpContext.GetOwinContext().Authentication.SignIn(new AuthenticationProperties() { IsPersistent = false }, id);
+            
+            return RedirectToLocal(returnUrl);
+
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            return Redirect("/");
+        }
+
+    }
+}
---
+++ UmbracoIdentityServer.Part3.Client/CustomRoleProvider.cs
@@ -0,0 +1,124 @@
+﻿using IdentityModel;
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading;
+using Umbraco.Web.Security.Providers;
+
+namespace UmbracoIdentityServer.Part3.Client
+{
+    public class CustomRoleProvider : MembersRoleProvider
+    {
+        #region Read Only Functions  
+        /// <summary>
+        /// read only in this provider
+        /// <see cref="RoleProvider.AddUsersToRoles"/>
+        /// </summary>
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new AccessViolationException("This Provider is Read-Only");
+        }
+
+        /// <summary>
+        /// read only in this provider
+        /// <see cref="RoleProvider.CreateRole"/>
+        /// </summary>
+        public override void CreateRole(string roleName)
+        {
+            throw new AccessViolationException("This Provider is Read-Only");
+        }
+
+        /// <summary>
+        /// read only in this provider
+        /// <see cref="RoleProvider.DeleteRole"/>
+        /// </summary>
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new AccessViolationException("This Provider is Read-Only");
+        }
+
+        /// <summary>
+        /// read only in this provider
+        /// <see cref="RoleProvider.RemoveUsersFromRoles"/>
+        /// </summary>
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new AccessViolationException("This Provider is Read-Only");
+        }
+        #endregion 
+
+        /// <summary>
+        /// return all the roles
+        /// </summary>
+        /// <returns>string array of portal security roles</returns>
+        public override string[] GetAllRoles()
+        {
+            return new[] { "Standard User" };
+        }
+
+        /// <summary>
+        /// returns the roles for a user
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns>string array of portal security roles</returns>
+        public override string[] GetRolesForUser(string username)
+        {
+            var userIdentity = (ClaimsIdentity)Thread.CurrentPrincipal.Identity;
+
+            var roles = userIdentity.Claims.Where(c => c.Type.Equals(JwtClaimTypes.Role)).Select(c => c.Value).ToArray();
+
+            return roles;
+        }
+
+        /// <summary>
+        /// checks user to see if they are in a role
+        /// 
+        /// returns true if the user is in the role listed
+        /// 
+        /// </summary>
+        /// <param name="username">name of user </param>
+        /// <param name="roleName">role name to search for</param>
+        /// <returns>true if they have the portal security role</returns>
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            string[] userRoles = GetRolesForUser(username);
+            return userRoles.Contains(roleName);
+        }
+
+        /// <summary>
+        /// checks if a role exsits
+        /// </summary>
+        /// <param name="roleName">name of role</param>
+        /// <returns>true if role is in the list</returns>
+        public override bool RoleExists(string roleName)
+        {
+            return GetAllRoles().Contains(roleName);
+        }
+
+        #region Not Implimented Functions
+        /// <summary>
+        /// Not Implimented in provider
+        /// <see cref="RoleProvider.GetUsersInRole"/>
+        /// </summary>
+        /// <param name="roleName">Name of Role</param>
+        /// <returns>List of Users in role</returns>
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Not Implimented in provider
+        /// <see cref="RoleProvider.FindUsersInRole"/>
+        /// </summary>
+        /// <param name="roleName">Name of Role</param>
+        /// <param name="usernameToMatch">UserName string to match</param>
+        /// <returns></returns>
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion 
+
+    }
+}
